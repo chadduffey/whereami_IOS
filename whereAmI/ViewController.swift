69069dd2f0dd38218798cc9b15e24ea3b68737dc
Chad Duffey <chadduffey@hotmail.com>
2015-05-06 10:58:40 +1000

    Initial Commit

---
+++ whereAmI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  whereAmI
+//
+//  Created by Chad Duffey on 6/05/2015.
+//  Copyright (c) 2015 Chad Duffey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
